183369eb06b33be7a395724f539a3b03f22d15e4
tess <tess@marina.home>
2025-02-01 19:37:36 +0000

    turn off JS codegen

--- src/index.js
+++ src/index.js
@@ -88,38 +88,38 @@ ws.addChangeListener(syncTypes)
 // This function resets the code and output divs, shows the
 // generated code from the workspace, and evals the code.
 // In a real application, you probably shouldn't use `eval`.
-const runCode = () => {
-  const code = javascriptGenerator.workspaceToCode(ws);
-  codeDiv.innerText = code;
+// const runCode = () => {
+//   const code = javascriptGenerator.workspaceToCode(ws);
+//   codeDiv.innerText = code;
 
-  outputDiv.innerHTML = '';
+//   outputDiv.innerHTML = '';
 
-  eval(code);
-};
+//   eval(code);
+// };
 
 // Load the initial state from storage and run the code.
 load(ws);
 // runCode();
 
 // Every time the workspace changes state, save the changes to storage.
-ws.addChangeListener((e) => {
-  // UI events are things like scrolling, zooming, etc.
-  // No need to save after one of these.
-  if (e.isUiEvent) return;
-  save(ws);
-});
+// ws.addChangeListener((e) => {
+//   // UI events are things like scrolling, zooming, etc.
+//   // No need to save after one of these.
+//   if (e.isUiEvent) return;
+//   save(ws);
+// });
 
-// Whenever the workspace changes meaningfully, run the code again.
-ws.addChangeListener((e) => {
-  // Don't run the code when the workspace finishes loading; we're
-  // already running it once when the application starts.
-  // Don't run the code during drags; we might have invalid state.
-  if (
-    e.isUiEvent ||
-    e.type == Blockly.Events.FINISHED_LOADING ||
-    ws.isDragging()
-  ) {
-    return;
-  }
-  runCode();
-});
+// // Whenever the workspace changes meaningfully, run the code again.
+// ws.addChangeListener((e) => {
+//   // Don't run the code when the workspace finishes loading; we're
+//   // already running it once when the application starts.
+//   // Don't run the code during drags; we might have invalid state.
+//   if (
+//     e.isUiEvent ||
+//     e.type == Blockly.Events.FINISHED_LOADING ||
+//     ws.isDragging()
+//   ) {
+//     return;
+//   }
+//   runCode();
+// });
